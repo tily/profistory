ad7e37045dff9ce4402307f8191ea7c8592a6527
tily <tily05@gmail.com>
2014-07-19 13:56:37 +0900

    update app

--- app.rb
+++ app.rb
@@ -1,25 +1,13 @@
 # coding: utf-8
-require 'sinatra'
-require 'omniauth-twitter'
-require 'mongoid'
-
-if ENV['MONGOHQ_URL']
-	uri = URI.parse(ENV['MONGOHQ_URL'])
-	database = Mongo::Connection.new(uri.host, uri.port).db(uri.path.gsub(/^\//, ''))
-	database.authenticate(uri.user, uri.password) unless (uri.user.nil? || uri.password.nil?)
-	Mongoid.database = database
-else
-	Mongoid.database = Mongo::Connection.new('localhost', Mongo::Connection::DEFAULT_PORT).db('nnade_development')
-end
+Bundler.require
 
 class User
 	include Mongoid::Document
-	include Mongoid::Timestamps # adds created_at and updated_at fields
+	include Mongoid::Timestamps
 	field :screen_name, :type => String
 	field :uid, :type => String
 	field :provider, :type => String
-	key :id
-	references_many :works
+	has_many :works
 	def self.create_with_omniauth(auth)
 		create! do |account|
 			account.provider = auth["provider"]
@@ -29,33 +17,76 @@ class User
 	end
 end
 
+class Work
+	include Mongoid::Document
+	include Mongoid::Timestamps
+	field :title, type: String
+	field :description, type: String
+	field :links_text, type: String
+	field :date, type: Time
+	validates :title, :length => {:maximum => 35}
+	validates :description, :length => {:maximum => 140}
+	validates :title, :presence => true
+	validates :links_text, :presence => true
+	validate do |work|
+		if links.flatten.any? {|link| !URI::regexp.match(link) }
+			work.errors.add(:link_text, "includes invalid URL(s)")
+		end
+		if links.flatten.size > 50
+			work.errors.add(:link_text, "includes more than 50 URLs")
+		end
+	end
+	belongs_to :user
+	def links
+		links_text.gsub(/\r/, '').split(/\n{2,}/).map {|x| x.split("\n") }
+	end
+end
+
 configure do
 	enable :sessions
+	set :session_secret, ENV['SESSION_SECRET']
+	set :haml, ugly: true, escape_html: true
+
 	use OmniAuth::Builder do
 		provider :twitter, ENV['CONSUMER_KEY'], ENV['CONSUMER_SECRET']
 	end
+
+	uri = URI.parse(ENV['MONGOHQ_URL'])
+	db = uri.path.gsub(/^\//, '')
+	connection = Mongo::Connection.new(uri.host, uri.port)
+	connection.db(db).authenticate(uri.user, uri.password) unless (uri.user.nil? || uri.password.nil?)
+	use Rack::Session::Mongo, :connection => connection, :db => db, :expire_after => 60*60*24*7 # 1 week
+
+	Mongoid.load!("./mongoid.yml")
+
+	TITLE = 'nnade'.split('').join(' ')
 end
 
 helpers do
 	def current_user
 		@current_user ||= User.where(uid: session[:uid]).first
 	end
+
+	def title
+		title = TITLE.dup
+		title << " > #{params[:screen_name]}" if params[:screen_name]
+		title << " > #{params[:title]}" if params[:title]
+		title
+	end
 end
 
 before do
 	pass if request.path_info =~ /^\/auth\//
-	redirect to('/auth/twitter') unless current_user
 end
 
 after do
-	response.body = haml env['sinatra.route'].split(' ').last.intern if response.status == 200
 end
 
 get '/auth/twitter/callback' do
 	auth = request.env["omniauth.auth"]
-	user = User.where(:provider => auth["provider"], :uid => auth["uid"]).first || User.create_with_omniauth(auth)
+	User.where(:provider => auth["provider"], :uid => auth["uid"]).first || User.create_with_omniauth(auth)
 	session[:uid] = auth["uid"]
-	redirect "http://#{request.env["HTTP_HOST"]}/#{user.screen_name}"
+	redirect "http://#{request.env["HTTP_HOST"]}/#{current_user.screen_name}"
 end
 
 get '/auth/failure' do
@@ -67,6 +98,173 @@ get '/logout' do
 end
 
 get '/' do
-	'Hello omniauth-twitter!'
+	haml :'/'
 end
 
+get '/:screen_name/:title/edit' do
+	@user = User.where(:screen_name => params[:screen_name]).first
+	@work = @user.works.where(:title => params[:title]).first
+	haml :'/:screen_name/:title/edit'
+end
+
+get '/:screen_name/:title' do
+	@user = User.where(:screen_name => params[:screen_name]).first
+	@work = @user.works.where(:title => params[:title]).first
+	haml :'/:screen_name/:title'
+end
+
+get '/:screen_name.json' do
+	content_type 'text/json'
+	@user = User.where(:screen_name => params[:screen_name]).first
+	JSON.pretty_generate JSON.parse jbuilder :'/:screen_name.json', layout: false
+end
+
+get '/:screen_name' do
+	@user = User.where(:screen_name => params[:screen_name]).first
+	@years = @user.works.map {|work| work.date.year }.uniq.sort.reverse
+	haml :'/:screen_name'
+end
+
+post '/:screen_name' do
+	@user = User.where(:screen_name => params[:screen_name]).first
+	halt 403 if @user != current_user
+	attributes =  {
+		title: params[:title],
+		description: params[:description],
+		links_text: params[:links_text],
+		date: params[:date]
+	}
+	if @work = current_user.works.where(:title => params[:old_title]).first
+		@work.update_attributes(attributes)
+	else
+		@work = current_user.works.create(attributes)
+	end
+	if @work.save
+		redirect URI.escape "http://#{request.env["HTTP_HOST"]}/#{current_user.screen_name}/#{@work.title}"
+	else
+		haml :'/:screen_name/:title/edit'
+	end
+end
+
+__END__
+@@ layout
+!!! 5
+%html
+	%head
+		%meta{charset: 'utf-8'}/
+		%title= title
+		%script{src:"http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js",type:"text/javascript"}
+		%script{src:"http://cdn.embed.ly/jquery.embedly-3.1.1.min.js",type:"text/javascript"}
+		%link{rel:'stylesheet',href:'http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css'}
+		:css
+			a,a:hover { color:blue }
+			img { max-width: 100% }
+	%body
+		%div.container
+			%p{style:"padding-top:1em;padding-bottom:0em"}
+				%strong
+					%a{href:'/'}= TITLE
+					- if @user
+						&nbsp;>&nbsp;
+						%a{href:"/#{@user.screen_name}"} #{@user.screen_name}
+					- if @work
+						&nbsp;>&nbsp;
+						= @work.title
+				%span{style:'float:right'}
+					- if current_user
+						%a{href:"/#{current_user.screen_name}"} my page
+						&nbsp;|&nbsp;
+						%a{href:'/logout'} logout
+					- else
+						%a{href:'/auth/twitter'} login
+			%hr
+			!= yield
+@@ /
+Create your portfolio with URL list.
+Contact @tily if any problem.
+@@ /:screen_name
+- if @user == current_user
+	%a{href:"/#{@user.screen_name}/*/edit"} add work
+%h1= @user.screen_name
+%div.row
+	- @years.each do |year|
+		%div.col-md-4
+			%h2= year
+			%ul
+				- @user.works.each do |work|
+					- if work.date.year == year
+						%li
+							%a{href:"/#{@user.screen_name}/#{work.title}",alt:work.description}= work.title
+@@ /:screen_name.json
+json.array!(@user.works) do |work|
+	json.title work.title
+	-# json.description work.description
+	json.date work.date
+	json.links work.links
+end
+@@ /:screen_name/:title
+- if @user == current_user
+	%a{href:"/#{@user.screen_name}/#{@work.title}/edit"} edit work
+%p
+	%h1
+		= @work.title
+		%small
+			= @work.description
+	%p= "#{@work.date.strftime('%Y-%m-%d')}"
+- @work.links.each_with_index do |links, i|
+	%div.row
+		- links.each do |link|
+			%div.col-md-3{style:'padding-bottom:10px'}
+				%a{href:link}= link
+	%hr
+:javascript
+	var width = $('div.row div').width()
+	$('div.row a').each(function(i, e) {
+		$(e).embedly({
+			endpoint: 'extract',
+			query: { maxwidth: width },
+			key: '19cf7d11f0d14c47b0625df7070823ad',
+			done: function(result) {
+				console.log(result)
+				if(result[0].type == 'image') {
+					$(e).html($('<img src="' + result[0].url + '">'))			  
+			 	} else if(result[0].media && result[0].media.html) {
+					$(e).html(result[0].media.html)
+				} else {
+					console.log(result[0].title)
+					$(e).html(result[0].title)
+					$(e).after('<br />', result[0].description)
+				}
+			}	
+		})
+	})
+@@ /:screen_name/:title/edit
+- if @work && !@work.errors.empty?
+	.alert.alert-danger
+		%ul
+			- @work.errors.each do |field, message|
+				%li= "#{field} #{message}"
+%form.form-horizontal{role:'form',method:'POST',action:"/#{@user.screen_name}"}
+	- if @work
+		%input.form-control{name:'old_title',type:'hidden',value:@work.title}
+	%div.form-group
+		%label.col-sm-2.control-label{for:'date'} date
+		%div.col-sm-2
+			%input.form-control{name:'date',type:'text',value:(@work.try(:date)||Time.now).strftime('%Y/%m/%d')}
+	%div.form-group
+		%label.col-sm-2.control-label{for:'title'} title
+		%div.col-sm-5
+			%input.form-control{name:'title',type:'text',value:@work.try(:title)}
+	%div.form-group
+		%label.col-sm-2.control-label{for:'description'} description
+		%div.col-sm-10
+			%input.form-control{name:'description',type:'text',value:@work.try(:description)}
+	%div.form-group
+		%label.col-sm-2.control-label{for:'links'} links
+		%div.col-sm-10
+			%span.help-block insert empty lines to group your links
+			%textarea.form-control{name:'links_text',rows:'25',style:'height: 1000'}
+				= @work.try(:links_text)
+	%div.form-group
+		%div.col-sm-offset-2.col-sm-10
+			%button{type:'submit',class:'btn btn-default'}= @work ? 'update' : 'add'
