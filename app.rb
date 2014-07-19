825a136ab84eb92dd52b2616dae3153b57e63666
tily <tily05@gmail.com>
2014-07-19 18:38:35 +0900

    set viewpoint

--- app.rb
+++ app.rb
@@ -152,6 +152,7 @@ __END__
 %html
 	%head
 		%meta{charset: 'utf-8'}/
+		%meta{name:"viewport",content:"width=device-width,initial-scale=1.0"}
 		%title= title
 		%script{src:"http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js",type:"text/javascript"}
 		%script{src:"http://cdn.embed.ly/jquery.embedly-3.1.1.min.js",type:"text/javascript"}
