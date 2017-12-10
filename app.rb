d8b4167efaea083ae1d63c7815abaeb2ddba39e3
tily <tily05@gmail.com>
2017-12-10 17:30:24 +0900

    separate configure block as file

--- app.rb
+++ app.rb
@@ -1,29 +1,7 @@
 Bundler.require
+require_relative './config/boot'
 require_relative './models/user'
 require_relative './models/work'
-
-configure do
-  register Config
-  enable :sessions
-  set :session_secret, Settings.session.secret
-  set :haml, ugly: true, escape_html: true
-  set :protection, :except => :path_traversal
-  case Settings.auth.provider
-  when "twitter"
-    use OmniAuth::Builder do
-      provider :twitter, Settings.auth.twitter.consumer_key, Settings.auth.twitter.consumer_secret
-    end
-  when "saml"
-    use OmniAuth::Strategies::SAML,
-      assertion_consumer_service_url: Settings.auth.saml.assertion_consumer_service_url,
-      issuer:                         Settings.auth.saml.issuer,
-      idp_sso_target_url:             Settings.auth.saml.idp_sso_target_url,
-      idp_cert_fingerprint:           Settings.auth.saml.idp_cert_fingerprint,
-      name_identifier_format:         Settings.auth.saml.name_identifier_format
-  end
-  use Rack::Session::Moneta, store: Moneta.new(:Mongo, host: "db")
-  Mongoid.load!("config/mongoid.yml")
-end
 
 helpers do
   def current_user
